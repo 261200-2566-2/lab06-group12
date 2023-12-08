84d270f164e992db37461288e556f0f54c8c534d
pubest <pubestpubest@gmail.com>
2023-12-08 19:38:16 +0700

    add weapon and Equipment

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,2 +1,13 @@
-public class Equipment {
+public abstract class Equipment {
+    //fields
+    protected String name;
+    protected int str,dex,def;
+    //methods
+    public String getName() {
+        return name;
+    }
+    public int getStr(){return str;}
+    public int getDex(){return dex;}
+    public int getDef() {return def;}
+    public abstract void statsDisplay(String type);
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -1,2 +1,11 @@
-public class Weapon {
+public abstract class Weapon {
+    protected int level;
+
+    public int getLevel() {return level;}
+    public void upLevel(){upLevel(1);}
+    public void upLevel(int lvl) {
+        level+=lvl;
+        updateStats();
+    }
+    protected abstract void updateStats();
 }
