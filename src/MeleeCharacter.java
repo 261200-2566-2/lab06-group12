e72082459a3e94cfd0967d1e9514da7c63293576
pubest <pubestpubest@gmail.com>
2023-12-08 19:32:58 +0700

    MeleeCharacter

--- src/MeleeCharacter.java
+++ src/MeleeCharacter.java
@@ -1,2 +1,11 @@
-public class MeleeCharacter {
+public abstract class MeleeCharacter extends Character{
+    protected Melee mainHand;
+    public MeleeCharacter(String name,int level,Melee mainHand){
+        super(name,level);
+        this.mainHand = new Melee();    //to be fixed
+    }
+    public MeleeCharacter(String name,int level){
+        super(name,level);
+        this.mainHand = new Melee();    //to be fixed
+    }
 }
